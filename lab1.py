e9c21656a41d58667dadddf203afea40c7784ef5
Scott <shur1@ualberta.ca>
2023-09-13 20:27:37 -0600

    Second commit

--- lab1.py
+++ lab1.py
@@ -3,5 +3,6 @@ import requests
 
 # print( requests.__version__ )
 
-resp = requests.get( "http://www.google.com" )
+#resp = requests.get( "http://www.google.com" )
+resp = requests.get( "https://raw.githubusercontent.com/shur1-uofa/cmput404-lab1/master/lab1.py" )
 print(resp.text)
